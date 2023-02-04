5f1dccdeecebac7a0d8e449558004639d71c49ab
stratoslav <stas.kurbanov03@gmail.com>
2023-02-04 16:55:36 +0200

    moved the logic to redux and added styles + made filter

--- to_do/src/components/ToDoListComponent.jsx
+++ to_do/src/components/ToDoListComponent.jsx
@@ -1,114 +1,97 @@
-import React, { useEffect, useState } from "react";
-import swal from "sweetalert";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { todoAction } from "../redux/todoSlice";
+import "../styles/todoList.scss";
 export const ToDoListComponent = () => {
-  let [todoText, setTodoText] = useState("");
-  let [completed, setCompleted] = useState(false);
-  let [notes, setNotes] = useState([]);
-
-  const handleClick = (e) => {
-    if (todoText !== "") {
-      const id = notes.length ? Number([...notes].pop().id) + 1 : 1;
-      let todo = {
-        id: id,
-        text: todoText,
-        completed: completed,
-        changeText: false,
-      };
-
-      setNotes([...notes, todo]);
-      setTodoText("");
-    }
-    return;
-  };
+  const dispatch = useDispatch();
+  let { todoText, todoList, filterTodo } = useSelector(
+    (state) => state.todoReducer
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
-  const deleteTodo = (id) => {
-    const newNote = notes.filter((note) => {
-      return note.id !== id;
-    });
-    setNotes(newNote);
-  };
-  const handleChangeCompleted = (id) => {
-    setNotes(
-      notes.map((note) => {
-        return note.id === id ? { ...note, completed: !note.completed } : note;
-      })
-    );
-  };
-  const searchNotes = (e) => {
-    console.log(e.target.value);
-    const filtered = notes.filter((note) =>
-      note.text.toLowerCase().includes(e.target.value)
-    );
-
-    setNotes(filtered);
-  };
-  const changeTodoText = (id, event) => {
-    if (event !== "") {
-      setNotes(
-        notes.map((note) => {
-          return note.id === id
-            ? { ...note, text: event, changeText: false }
-            : note;
-        })
-      );
-    }
-    swal("You should enter at least one symbol!");
-  };
-  const isChangedText = (id, event) => {
-    if (event === "P") {
-      setNotes(
-        notes.map((note) => {
-          return note.id === id ? { ...note, changeText: true } : note;
-        })
-      );
-    }
-  };
   return (
     <div className="todo">
-      <h1>To Do List</h1>
-      <h4>If you want to change text, just click on it</h4>
-      <form onSubmit={handleSubmit}>
-        <label className="">
+      <h1 className="todo__title">To Do List</h1>
+      <h4 className="todo__title-description">
+        If you want to change text, just click on it
+      </h4>
+      <form className="todo__form" onSubmit={handleSubmit}>
+        <label className="todo__form-label">
           Write
           <input
-            onChange={(e) => setTodoText(e.currentTarget.value)}
+            onChange={(e) => dispatch(todoAction.addText(e.target.value))}
             value={todoText}
-            className=""
+            className="todo__form-input"
             name="todo"
             type="text"
             placeholder="notification"
           />
         </label>
-        <button onClick={handleClick} type="submit">
+        <button
+          className="todo__form-button"
+          onClick={(e) => dispatch(todoAction.handleClick())}
+          type="submit"
+        >
           Send
         </button>
-        <input type="text" onChange={searchNotes} />
+        <input
+          className="todo__form-input"
+          type="text"
+          onChange={(e) => dispatch(todoAction.searchNotes(e.target.value))}
+        />
       </form>
-      <div>
-        {notes.map(({ id, text, completed, changeText }) => (
-          <div>
-            {changeText === false ? (
-              <p onClick={(e) => isChangedText(id, e.target.nodeName)} key={id}>
-                {text}
-              </p>
-            ) : (
-              <input
-                type="text"
-                onBlur={(e) => changeTodoText(id, e.target.value)}
-              />
-            )}
-            <button onClick={() => deleteTodo(id)}>Delete</button>
-            <input
-              type="checkbox"
-              checked={completed}
-              onChange={() => handleChangeCompleted(id)}
-            />
-          </div>
-        ))}
+      <div className="todo__list">
+        {todoList
+          .filter((note) => note.text.toLowerCase().includes(filterTodo))
+          .map(({ id, text, completed, toggleChangeText }) => (
+            <div className="todo__list-wrap">
+              {toggleChangeText === false ? (
+                <p
+                  className="todo__list-text"
+                  onClick={(e) =>
+                    dispatch(
+                      todoAction.isChangedText({ id, event: e.target.nodeName })
+                    )
+                  }
+                  key={id}
+                >
+                  {text}
+                </p>
+              ) : (
+                <input
+                  className="todo__list-input"
+                  type="text"
+                  onBlur={(e) =>
+                    dispatch(
+                      todoAction.changeTodoText({ id, event: e.target.value })
+                    )
+                  }
+                />
+              )}
+              <button
+                className="todo__list-button"
+                onClick={() => dispatch(todoAction.deleteTodo(id))}
+              >
+                Delete
+              </button>
+              <div className="checkbox">
+                <input
+                  id={id}
+                  className="todo__list-checkbox checkbox-item"
+                  type="checkbox"
+                  checked={completed}
+                  onChange={() =>
+                    dispatch(todoAction.handleChangeCompleted(id))
+                  }
+                />
+                <label for={id}></label>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
---
+++ to_do/src/redux/todoSlice.js
@@ -0,0 +1,82 @@
+import { createSlice } from "@reduxjs/toolkit";
+import swal from "sweetalert";
+
+let initialState = {
+  todoText: "",
+  todoList: [],
+  completed: false,
+  toggleChangeText: false,
+  filterTodo: "",
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: initialState,
+  reducers: {
+    addText(state, { payload }) {
+      state.todoText = payload;
+    },
+
+    handleClick(state, action) {
+      if (state.todoText !== "") {
+        const id = state.todoList.length
+          ? Number([...state.todoList].pop().id) + 1
+          : 1;
+        let todo = {
+          id: id,
+          text: state.todoText,
+          completed: state.completed,
+          toggleChangeText: state.toggleChangeText,
+        };
+
+        state.todoList.push({ ...todo });
+        state.todoText = "";
+      }
+      return;
+    },
+
+    deleteTodo(state, { payload }) {
+      let newNote = state.todoList.filter((note) => {
+        return note.id !== payload;
+      });
+      state.todoList = newNote;
+    },
+
+    handleChangeCompleted(state, { payload }) {
+      let isCompleted = state.todoList.find((note) => {
+        return note.id === payload;
+      });
+      isCompleted.completed = !isCompleted.completed;
+    },
+
+    isChangedText(state, { payload }) {
+      if (payload.event === "P") {
+        const changeText = state.todoList.find((note) => {
+          return note.id === payload.id;
+        });
+
+        changeText.toggleChangeText = true;
+      }
+    },
+
+    changeTodoText(state, { payload }) {
+      if (payload.event !== "") {
+        const changeText = state.todoList.find((note) => {
+          return note.id === payload.id;
+        });
+
+        changeText.toggleChangeText = !changeText.toggleChangeText;
+        changeText.text = payload.event;
+
+        return;
+      }
+      swal("You should enter at least one symbol!");
+    },
+
+    searchNotes(state, { payload }) {
+      state.filterTodo = payload;
+    },
+  },
+});
+
+export const { reducer: todoReducer, actions: todoAction } = todoSlice;
